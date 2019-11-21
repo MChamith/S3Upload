81dbd8593e42beb07786f20da8a18b5a18e0b242
MChamith <mawelacm@gmail.com>
2019-11-21 19:22:37 +0530

    add new line
    Signed-off-by: MChamith <mawelacm@gmail.com>

--- create_file_list.py
+++ create_file_list.py
@@ -1,17 +1,18 @@
 import os
 
+print('creating training dataset')
 for root, dirs, filenames in os.walk('/home/ec2-user/dataset/SiW_release/Train'):
     for file in filenames:
         if file.endswith('.mov'):
             path = os.path.join(root, file)
-
+            print(file + ' ' + str(path.split('/')[-3]))
             with open('train.csv', 'a') as fwriter:
-                fwriter.write(file + ' ' + str(path.split('/')[-3]))
-
+                fwriter.write(file + ' ' + str(path.split('/')[-3]) + '\n')
+print('creating test dataset ')
 for root, dirs, filenames in os.walk('/home/ec2-user/dataset/SiW_release/Test'):
     for file in filenames:
         if file.endswith('.mov'):
             path = os.path.join(root, file)
-
+            print(file + ' ' + str(path.split('/')[-3]))
             with open('test.csv', 'a') as fwriter:
-                fwriter.write(file + ' ' + str(path.split('/')[-3]))
+                fwriter.write(file + ' ' + str(path.split('/')[-3]) + '\n')
