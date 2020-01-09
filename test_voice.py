5dfc138850f4ca331a3025aeb5314c9887b441e5
MChamith <mawelacm@gmail.com>
2020-01-09 14:14:24 +0530

    new enroll
    Signed-off-by: MChamith <mawelacm@gmail.com>

--- test_voice.py
+++ test_voice.py
@@ -24,10 +24,10 @@ def enroll():
                     path = os.path.join(root, file)
                     count += 1
                     speakerid = path.split('/')[-3]
-                    print(speakerid)
+                    # print(speakerid)
                     upload_file(path, 'ustlkcomdev-test-biometric',
-                                'meetsid/' + str(speakerid) + '/voice/voice.m4a')
-                    data = {"file_path": 'meetsid/' + str(speakerid) + '/voice/voice.m4a', "wallet_id": str(speakerid)}
+                                'meetsid/' + str(speakerid) + '_1/voice/voice.m4a')
+                    data = {"file_path": 'meetsid/' + str(speakerid) + '_1/voice/voice.m4a', "wallet_id": str(speakerid)+'_1'}
                     response = requests.post(enroll_url,
                                              headers={'content-type': 'application/json'},
                                              data=json.dumps(data))
@@ -45,26 +45,31 @@ def verify():
         for spk_dir in sub_parent_dirs:
             for root, dirs, filenames in os.walk(spk_dir):
                 for file in filenames:
-                    print(file)
+                    # print(file)
                     if file.endswith('.m4a'):
                         path = os.path.join(root, file)
+                        path = path.split('test-clean')[-1]
+                        # path = '/' + path.split('/')[1] + '/' + path.split('/')[3]
+                        # speakerid = path.split('/')[-3]
+                        print(path)
 
-                        speakerid = path.split('/')[-3]
-                        print(speakerid)
-                        upload_file(path, 'ustlkcomdev-test-biometric',
-                                    'meetsid/' + str(speakerid) + '/verify/' + str(file))
+                        # upload_file(path, 'ustlkcomdev-test-biometric',
+                        #             'meetsid/' + str(speakerid) + '/verify/' + str(file))
                         for spkr in speakerids:
-                            data = {"file_path": 'meetsid/' + str(speakerid) + '/verify/' + str(file),
-                                    "wallet_id": str(spkr)}
-                            response = requests.post(verify_url,
-                                                     headers={'content-type': 'application/json'},
-                                                     data=json.dumps(data))
-                            result = response.json()
-                            with open('libri_test_clean.csv', 'a') as writer:
+                            with open('testclean.csv', 'a') as writer:
                                 writer.write(
-                                    str(speakerid) + ',' + str(spkr) + ',' + result['matching'] + ',' + str(
-                                        result['score']) + ',' + str(path)+'\n')
-                            print(response.json())
+                                    str(spkr) + ',' + 'meetsid/' + path.split('/')[1] + '/verify/' + path.split('/')[3] + '\n')
+                        #     data = {"file_path": 'meetsid/' + str(speakerid) + '/verify/' + str(file),
+                        #             "wallet_id": str(spkr)}
+                        #     response = requests.post(verify_url,
+                        #                              headers={'content-type': 'application/json'},
+                        #                              data=json.dumps(data))
+                        #     result = response.json()
+                        #     with open('libri_test_clean.csv', 'a') as writer:
+                        #         writer.write(
+                        #             str(speakerid) + ',' + str(spkr) + ',' + result['matching'] + ',' + str(
+                        #                 result['score']) + ',' + str(path)+'\n')
+                        #     print(response.json())
 
 
-verify()
+enroll()
